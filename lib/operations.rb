b73b9bcd14eabbff6023284d33e2d7c21f11cc2d
confident-token-1019 <theoonetj@gmail.com>
2019-12-14 06:25:30 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,7 +10,7 @@ end
 
 
 def not_safe?(speed)
-  speed < 40 || speed >60? "Going 30 mph on the freeway would be unsafe, as would going 95 mph" :
+  speed < 40 || speed >60? "Going 30 mph on the freeway would be unsafe, as would going 95 mph" : "safe range"
 	
 end
 	
