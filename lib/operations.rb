2ea1a2fee31e7e50e54e0ad105395964812d1969
confident-token-1019 <theoonetj@gmail.com>
2019-12-14 06:22:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,16 @@
 def unsafe?(speed)
 
-spseed = 50
+
 if spseed < 40 || spseed >60
   return "Going 30 mph on the freeway would be unsafe, as would going 95 mph."
+else 
+  return "safe range"
 end
 
 
 
 def not_safe?(speed)
+  speed < 40 || speed >60? "Going 30 mph on the freeway would be unsafe" :
 	
 end
 	
